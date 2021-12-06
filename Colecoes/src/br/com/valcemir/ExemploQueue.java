9ec818ec143e6be162dc8a2f3f7b15ed78f82c40
Valcemir01 <valcemirads@gmail.com>
2021-12-06 20:18:23 -0300

    Classe de exemplo de fila

--- Colecoes/src/br/com/valcemir/ExemploQueue.java
+++ Colecoes/src/br/com/valcemir/ExemploQueue.java
@@ -6,5 +6,29 @@ import java.util.Queue;
 public class ExemploQueue {
     public static void main(String[] args) {
         Queue<String> filaBanco = new LinkedList<>();
+        filaBanco.add("Juliana");
+        filaBanco.add("Pedro");
+        filaBanco.add("Carlos");
+        filaBanco.add("Larissa");
+        filaBanco.add("João");
+
+        //PERCORRE A FILA
+        for (String item : filaBanco){
+            System.out.println(item);
+        }
+        //RETONA O PRIMEIRO ITEM DA FILA SEM REMOVER
+        System.out.println(filaBanco.peek());
+        //RETORNA O PRIMEIRO ITEM DA FILA E O REMOVE
+        System.out.println(filaBanco.poll());
+        //ADIVIONA NOVO ITEM A FILA
+        filaBanco.add("Daniel");
+        //VERIFICA TAMANHO DA FILA
+        System.out.println(filaBanco.size());
+        //VERIFICA SE A FILA ESTA VAZIA
+        System.out.println(filaBanco.isEmpty());
+        //VERIFICA SE UM ITEM EXISTI NA FILA
+        System.out.println(filaBanco.contains("Daniel"));
+
+        System.out.println(filaBanco);
     }
 }
