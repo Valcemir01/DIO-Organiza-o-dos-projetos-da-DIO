67253b38029699d4b0e982637f816fa635f62224
Valcemir01 <valcemirads@gmail.com>
2021-12-07 20:55:21 -0300

    Classe de exemplo de Stream

--- Colecoes/src/br/com/valcemir/ExemploStream.java
+++ Colecoes/src/br/com/valcemir/ExemploStream.java
@@ -1,2 +1,28 @@
-package br.com.valcemir;public class ExemploStream {
+package br.com.valcemir;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class ExemploStream {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Valcemir");
+        nomes.add("Alice");
+        nomes.add("Alexandre");
+        nomes.add("Sofia");
+        nomes.add("Isabele");
+
+
+        //RETORNA O TAMANHO DA LISTA
+        System.out.println(nomes.stream().count());
+
+        //RETORNA SOMENTE OS ITEM QUER CONTEM A LETRA C
+        System.out.println("Item com a letra C " + nomes.stream().filter((nome)-> nome.toLowerCase().contains("c"))
+                .collect(Collectors.toList()));
+
+        //RETORNA OS NUMERO INDICA DA PARTE DE CIMA DA LISTA
+        System.out.println(nomes.stream().limit(4).collect(Collectors.toList()));
+    }
 }
